9d94748a5b89c5394ed43916e8f3363958ce7893
Hdez_J <jlopezhdez036@gmail.com>
2026-02-01 19:18:37 +1000

    numpy

---
+++ Python/1. Numpy1.py
@@ -0,0 +1,95 @@
+'''
+At the core of the NumPy package, is the array, called ndarray, object. 
+This encapsulates n-dimensional arrays of homogeneous data types.
+
+Vectorization: element-by-element operations 
+'''
+
+import numpy as np
+
+# Array creation
+
+# From another python object
+
+a = np.array(list(range(2,5)))
+
+# The type of the array can also be explicitly specified
+
+b = np.array([i*2 for i in range(1,11)], dtype = float)
+b
+
+# Intrinsic numpy array creation functons
+
+# 1D array
+
+np. arange(1,20,2) #start, stop, step
+
+np.linspace(0,2,9) # start, stop, num
+
+# 2D arrya creation
+
+np.eye(2) # Identity matrix
+
+np.diag([1,2,3]) # diagonal elements
+
+np.vander(np.arange(1,6)) # vandermonde matrix
+
+
+# General array creation functions
+
+np.zeros([2,2])
+
+np.ones([3,3])
+
+np.random.default_rng(42).random((2,3)) # values between 0-1
+
+np.random.default_rng(42).random((2,3,2)) # 3 dimensions
+
+np.indices((3,3))
+
+
+# Linspace is useful when you can evaluate a function
+
+x = np.linspace(0, 2*np.pi,100)
+f = np.sin(x)
+
+import matplotlib.pyplot as plt
+
+plt.plot(f)
+
+####### More important attributes of an ndarray #######
+
+a.ndim # number of axes or dimensions
+
+a.shape #dimensions of the arrat. This is a tuple of integers
+
+a.size # Total number of elements
+
+a.dtype # type of element of the array
+
+
+####### More important attributes of an ndarray #######
+
+# Arithmetic operators on arrays apply elementwise
+
+a = np.linspace(1,10,4)
+b = np.arange(4)
+
+a - b, a+b, a*b, a//b
+
+b *=a # Operation to modify an existing array
+
+# Matriz operations
+
+A = np.array([[1, 1],
+              [0, 1]])
+B = np.array([[2, 0],
+              [3, 4]])
+
+A@B # Matriz product
+
+A.dot(B) # Another way to do a matrix product
+
+
+
+
