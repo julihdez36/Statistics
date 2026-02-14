adb44746802733063950ea66b825580103db1ac5
Hdez_J <jlopezhdez036@gmail.com>
2026-02-14 23:39:13 +1000

    prob

--- Python/Large numbers.py
+++ Python/Large numbers.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 # Law of large numbers
 
 import numpy as np
@@ -6,22 +7,21 @@ import matplotlib.pyplot as plt
 
 np.random.seed(2131982)
 
-# Ejemplo ley de grandes números
-# moneda p=1/2 cara=1 seca=0
 
 resultados = []
 
-np.random.choice(['Heads','Tails'], 1)
+np.random.choice(['Heads','Tails'], 5)
 
-for i in range(1,1000):
+for i in range(1,10000):
     lanzamientos = np.random.choice([0,1], i)
-    caras = lanzamientos.mean() # Proporción de apairición de uno
+    caras = lanzamientos.mean() # Proportion
     resultados.append(caras)
 
 
 
-plt.plot(resultados, c = 'r')
+plt.plot(resultados, c = 'r', linewidth =.4)
 plt.axhline(0.5)
+plt.title('Law of large numbers')
 plt.xlabel("Number of dice rolls")
 plt.ylabel("Frequency of faces")
 plt.grid(linestyle = '--')
