7ceeb5968790967d7245231a8bcb06cb1f7542bb
Hdez_J <jlopezhdez036@gmail.com>
2026-02-14 23:47:22 +1000

    prob

--- Python/Large numbers.py
+++ Python/Large numbers.py
@@ -5,7 +5,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(2131982)
+np.random.seed(42)
 
 
 resultados = []
@@ -27,3 +27,20 @@ plt.ylabel("Frequency of faces")
 plt.grid(linestyle = '--')
 plt.show()
 
+# Changing probabilities
+
+resultados = []
+
+for i in range(1,10000):
+    lanzamientos = np.random.choice([0,1], i, p = [.8,.2])
+    caras = lanzamientos.mean() # Proportion
+    resultados.append(caras)
+
+plt.plot(resultados, c = 'r', linewidth =.4)
+plt.axhline(0.5, c = 'purple')
+plt.axhline(0.2, c = 'b')
+plt.title('Law of large numbers')
+plt.xlabel("Number of dice rolls")
+plt.ylabel("Frequency of faces")
+plt.grid(linestyle = '--')
+plt.show()
